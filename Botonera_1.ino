cf5d59b0f9a18a5b4a8119d641e62137a87830d2
manueloriano <manuelalejandrogarcianavarrete@gmail.com>
2023-05-17 18:10:20 -0600

    Cambiando para usar convertidor de 4 bits

--- Botonera_1.ino
+++ Botonera_1.ino
@@ -1,66 +1,68 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_LEDBackpack.h>
-#include <Adafruit_MCP23X17.h>
+// Librería para controlar el display de 7 segmentos
 
-Adafruit_7segment matrix = Adafruit_7segment();
-Adafruit_MCP23X17 mcp1;
-Adafruit_MCP23X17 mcp2;
+#include <SevSeg.h>
 
+// Pines de los botones y LEDs
 const int numButtons = 8;
-const int player1Buttons[numButtons] = {0, 1, 2, 3, 4, 5, 6, 7};
-const int player2Buttons[numButtons] = {8, 9, 10, 11, 12, 13, 14, 15};
+const int player1Buttons[numButtons] = {2, 3, 4, 5, 6, 7, 8, 9};
+const int player2Buttons[numButtons] = {10, 11, 12, 13, A0, A1, A2, A3};
+
+const int player1LEDs[numButtons] = {A4, A5, A6, A7, A8, A9, A10, A11};
+const int player2LEDs[numButtons] = {A12, A13, A14, A15, 16, 17, 18, 19};
+
+// Variables para almacenar los puntajes
 
 int player1Score = 0;
 int player2Score = 0;
 
+// Configuración del display de 7 segmentos
+
+SevSeg sevseg;
+
 void setup() {
-  matrix.begin(0x70);  // Inicializar el display de 7 segmentos
-  matrix.setBrightness(7); // Ajustar el brillo del display
+  // Inicialización del display de 7 segmentos
+  byte hardwareConfig = COMMON_CATHODE;
+  sevseg.begin(hardwareConfig, 4, player1Score, player2Score);
 
-  mcp1.begin_I2C(0); // Inicializar el convertidor MCP23017 para los botones del jugador 1
-  mcp2.begin_I2C(1); // Inicializar el convertidor MCP23017 para los botones del jugador 2
-
+  // Configuración de los pines de los botones y LEDs
   for (int i = 0; i < numButtons; i++) {
-    mcp1.pinMode(player1Buttons[i], INPUT_PULLUP); // Configurar los pines de los botones del jugador 1 como entrada con resistencias pull-up internas en el convertidor MCP23017
-    mcp2.pinMode(player2Buttons[i], INPUT_PULLUP); // Configurar los pines de los botones del jugador 2 como entrada con resistencias pull-up internas en el convertidor MCP23017
+    pinMode(player1Buttons[i], INPUT_PULLUP);
+    pinMode(player2Buttons[i], INPUT_PULLUP);
+
+    pinMode(player1LEDs[i], OUTPUT);
+    pinMode(player2LEDs[i], OUTPUT);
   }
 }
 
 void loop() {
-  // Obtener un índice de botón aleatorio para cada jugador
-  int player1ButtonIndex = random(numButtons);
-  int player2ButtonIndex = random(numButtons);
+  // Generar índices de botones aleatorios para cada jugador
+  int player1Index = random(0, numButtons);
+  int player2Index = random(0, numButtons);
 
-  // Encender los LEDs de los botones correspondientes
-  mcp1.digitalWrite(player1Buttons[player1ButtonIndex], LOW);
-  mcp2.digitalWrite(player2Buttons[player2ButtonIndex], LOW);
+  // Encender los LEDs correspondientes a los botones seleccionados
+  digitalWrite(player1LEDs[player1Index], HIGH);
+  digitalWrite(player2LEDs[player2Index], HIGH);
 
-  // Mostrar los índices de los botones en el display de 7 segmentos
-  matrix.print(player1ButtonIndex);
-  matrix.writeDigitNum(1, player2ButtonIndex);
-  matrix.writeDisplay();
+  // Mostrar los índices de los botones seleccionados en el display de 7 segmentos
+  sevseg.setNumber(0, player1Index);
+  sevseg.setNumber(1, player2Index);
 
-  // Esperar a que los jugadores presionen los botones correspondientes
-  while (!mcp1.digitalRead(player1Buttons[player1ButtonIndex]) || !mcp2.digitalRead(player2Buttons[player2ButtonIndex])) {}
+  // Esperar a que los jugadores presionen los botones
+  while (digitalRead(player1Buttons[player1Index]) == HIGH || digitalRead(player2Buttons[player2Index]) == HIGH) {
+    // Esperar
+  }
 
-  // Apagar los LEDs de los botones
-  mcp1.digitalWrite(player1Buttons[player1ButtonIndex], HIGH);
-  mcp2.digitalWrite(player2Buttons[player2ButtonIndex], HIGH);
+  // Apagar los LEDs
+  digitalWrite(player1LEDs[player1Index], LOW);
+  digitalWrite(player2LEDs[player2Index], LOW);
 
   // Incrementar el puntaje del jugador correspondiente
-  if (mcp1.digitalRead(player1Buttons[player1ButtonIndex])) {
-    player1Score++;
-  }
+  player1Score++;
+  player2Score++;
 
-  if (mcp2.digitalRead(player2Buttons[player2ButtonIndex])) {
-    player2Score++;
-  }
+  // Actualizar el display de 7 segmentos con los nuevos puntajes
+  sevseg.setNumber(player1Score, 1);
+  sevseg.setNumber(player2Score, 0);
 
-  // Mostrar el puntaje en el display de 7 segmentos
-  matrix.print(player1Score);
-  matrix.writeDigitNum(1, player2Score);
-  matrix.writeDisplay();
-
-  // Esperar un breve tiempo antes de mostrar el próximo cambio de botones
   delay(1000);
 }
